eceff19bd4194a7225620e33ada9f39e5584b3b8
sebasoviedo69 <sebasoviedo69@hotmail.com>
2021-09-27 14:35:55 -0500

    feat: navbar

---
+++ public/js/menu.js
@@ -0,0 +1,12 @@
+/*
+function stopIt (e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+var a = document.querySelectorAll('.nav-link');
+for (var i = 0; i < a.length; i++) {
+    a[i].classList.add('external');
+    a[i].removeEventListener("click", stopIt);
+}
+ */
