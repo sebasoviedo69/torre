8874205fab7b2e282d1b5a1eb6385796a91faec3
sebasoviedo69 <sebasoviedo69@hotmail.com>
2021-09-27 08:36:02 -0500

    feat: read and generate image

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const request = require("request");
 const jimp = require("jimp");
+//const fetch = require("node-fetch");
 
 const info = {
   id: "",
@@ -21,19 +22,31 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
+async function GetUserInfo(userId) {
+
+  /*fetch('http://example.com/movies.json')
+  .then(response => response.json())
+  .then(data => console.log(data));*/
+  var userPictureUrl = "https://res.cloudinary.com/torre-technologies-co/image/upload/v0/origin/starrgate/users/profile_c68ab3f19fa618f6b0ead245ad5b67482830d68f.jpg";
+  console.log(userPictureUrl);
+  return userPictureUrl;
+}
+
 // generate image
 async function ProcessImage(info) {
+
   const imgName = info.id + '_result.png';
-  const imgReadPath = path.join(__dirname, 'public/imgs/');
+  const imgReadPath =  await GetUserInfo(info.id)
   const imgSavePath = path.join(__dirname, 'public/imgs/result/', imgName);
   const srcImgPath = path.join('/imgs/result/', imgName);
 
-  const image = await jimp.read(path.join(imgReadPath, info.type));
-  
-  image.blur(2, function(err){
-    if (err) throw err;
-  })
-  .write(imgSavePath);
+
+    const image = await jimp.read(imgReadPath);
+    image.blur(2, function(err){
+      if (err) throw err;
+    })
+    .write(imgSavePath);
 
   return srcImgPath;
 }
@@ -58,6 +71,6 @@ app.post('/results', async function(req, res){
 });
 
 // run the server
-var listener = app.listen(process.env.PORT || 80, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("listening on port " + listener.address().port);
 });
