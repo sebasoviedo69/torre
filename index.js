b20d63c9e086a9a902f971bc07d2573f08c82fcf
sebasoviedo69 <sebasoviedo69@hotmail.com>
2021-09-27 09:59:33 -0500

    feat: get, process and render image

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const request = require("request");
 const jimp = require("jimp");
-//const fetch = require("node-fetch");
 
 const info = {
   id: "",
-  type: ""
+  picture: ""
 
 };
 
@@ -22,31 +21,39 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
+// get user picture from torre
 async function GetUserInfo(userId) {
+  var torreUrl = 'https://torre.bio/api/bios/'
+  var pictureUrl = "https://res.cloudinary.com/torre-technologies-co/image/upload/v0/origin/starrgate/users/profile_c68ab3f19fa618f6b0ead245ad5b67482830d68f.jpg";
+  
+  request(torreUrl+userId, function (error, response, body){
 
-  /*fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));*/
-  var userPictureUrl = "https://res.cloudinary.com/torre-technologies-co/image/upload/v0/origin/starrgate/users/profile_c68ab3f19fa618f6b0ead245ad5b67482830d68f.jpg";
-  console.log(userPictureUrl);
-  return userPictureUrl;
+    body = JSON.parse(body);
+    console.log(userId);
+
+    if(body.code !== undefined && body.code == "011002"){
+      return res.json({"success": false, "msg": "Error, user not found."});
+    }else{ 
+      pictureUrl = body.person.picture;
+      //return pictureUrl;
+    }
+  });
+  return pictureUrl;
+
 }
 
 // generate image
 async function ProcessImage(info) {
 
   const imgName = info.id + '_result.png';
-  const imgReadPath =  await GetUserInfo(info.id)
+  const imgReadPath = info.picture;
   const imgSavePath = path.join(__dirname, 'public/imgs/result/', imgName);
   const srcImgPath = path.join('/imgs/result/', imgName);
-
-
-    const image = await jimp.read(imgReadPath);
-    image.blur(2, function(err){
-      if (err) throw err;
-    })
-    .write(imgSavePath);
+  const image = await jimp.read(imgReadPath);
+  image.blur(2, function(err){
+    if (err) throw err;
+  })
+  .write(imgSavePath);
 
   return srcImgPath;
 }
@@ -63,11 +70,14 @@ app.post('/results', async function(req, res){
 
   const userInfo = Object.create(info);
   userInfo.id = req.body.user.id;
-  userInfo.type = "mu-alien.jpg";
 
-  var imgPath = await ProcessImage(userInfo);
-  console.log(imgPath);
-  res.render('results', {imgPath: imgPath});
+  if (userInfo.id != undefined){
+    
+    userInfo.picture = await GetUserInfo(userInfo.id);
+
+    var imgPath = await ProcessImage(userInfo);
+    res.render('results', {imgPath: imgPath});
+  }
 });
 
 // run the server
