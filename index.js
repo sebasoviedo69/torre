fd9224cd70987dc3e76e6f04bfcfda4de2001c3f
sebasoviedo69 <sebasoviedo69@hotmail.com>
2021-09-26 19:13:48 -0500

    feat: css, ejs files and post

--- index.js
+++ index.js
@@ -7,14 +7,27 @@ const request = require("request");
 
 // app setup
 const app = express();
-app.use(morgan("combined"));
+//app.use(morgan("combined"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // routes
+
+// home 
 app.get(['/', '/home', 'inicio', 'index'], function (req, res) { 
+  
   res.render('index');
+});
+
+// results
+app.post('/results', function(req, res) {
+
+  console.log("req.params.user");
+  console.log(req.body.user);
+
 });
 
 // run the server
