057198c4b12d780a9521c02c08584b7a71c601f0
sebasoviedo69 <sebasoviedo69@hotmail.com>
2021-09-27 21:52:02 -0500

    fix: azure deploy - change name of files

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post('/results', async function(req, res){
   } catch (errorCode) {
 
     msg = HandleErrors(errorCode);
-    res.render('error', {msg: msg});
+    res.render('default', {msg: msg});
   }
 });
 
